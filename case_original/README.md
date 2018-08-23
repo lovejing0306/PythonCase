86ea113bd1f6ab30e30bebeece8523da344762d2
lovejing0306 <lovejing0306@gmail.com>
2018-08-23 11:29:41 +0800

    add process

--- case_original/README.md
+++ case_original/README.md
@@ -1,3 +1,5 @@
 # python中的多线程
   * 使用thread方式实现的多线程
   * 使用threading方式实现的多线程
+  * 使用multiprocessing中的process模快创建单线程
+  * 使用multiprocessing中的pool模块创建线程池
---
+++ case_original/multi_process_process.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+import os
+from multiprocessing import Process
+
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    # 创建单个进程
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
